001152575b5ce5a9627e5cc1cbff0bbb8cc82e85
alex <dong,xingbin,alex@gmail.com>
2017-12-09 17:31:47 +0800

    upload

--- src/views/nav1/Page1.vue
+++ src/views/nav1/Page1.vue
@@ -138,7 +138,8 @@
                             :on-preview="handlePreview"
                             :on-remove="handleRemove"
                             :file-list="fileList"
-                            :auto-upload="false">
+                            :auto-upload="false"
+                             multiple>
                         <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
                         <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
                         <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
@@ -717,7 +718,8 @@
                 console.log(file, fileList);
             },
             handlePreview(file) {
-                console.log(file);
+                console.log('handlePreview: ' + file);
+
             },
             ajaxFileUpload(){
                 $(".file").on("change", "input[type='file']", function () {
